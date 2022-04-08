70abb4ec7022343c6469f1f4254684bef85c13fa
RhalisK <khalis.rabehi@etu.univ-lyon1.fr>
2022-04-08 17:14:01 +0200

    Update Test_site.ino

--- Serveur/Test_site/Test_site.ino
+++ Serveur/Test_site/Test_site.ino
@@ -1,12 +1,6 @@
 #include <WiFi.h>  // Librairie Wifi.h
 #include <WebServer.h>  // Librairie WebServer.h
-#include <BLEDevice.h>
-#include "site.h"
-#include <BLEUtils.h>
-#include <BLEServer.h>#include "FeatherShieldPinouts.h"
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 const char* ssid = "IPhone de Bob Rasowski";
 const char* password = "azerty01";
@@ -17,7 +11,83 @@ WebServer server(80);
 char texteEtatIR[2][10] = {"Libre", "Occupé"}; // Affichage ETEINTE ou ALLUMEE
 
 void handleRoot() {  // Début de la page HTML
-  server.setContentLength(page.length());  // Permet l'affichage plus rapide après chaque clic sur les boutons
+  const char page[] PROGMEM = R"rawliteral(<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="./style.css" rel="stylesheet">
+    <title>Manager</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+        
+        *{
+            font-family:'Roboto', sans-serif;;
+            margin: 0;
+            transition: 2s linear;
+        }
+        
+        hr{
+            background-color: black;
+            height : 5px;
+            margin : 0;
+        }
+        
+        .top{
+            background-color: rgb(13, 133, 239);
+            text-align: center;
+            height : 10rem
+        }
+        
+        .top h1 {
+            padding-top: 15%;
+        }
+        
+        .mid{
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            background-color: rgb(222, 220, 220);
+            height : 400px ;
+        }
+        
+        footer{
+            background-color: black;
+            width : 100%;
+            height: 100px;
+        }
+        
+        .card{
+            background-color: rgb(202, 200, 200);
+            height : 60%;
+            text-align: center;
+            width : 160px;
+            border-radius: 5%;
+        }
+        
+        .card h2{
+            padding-top: 5%;
+}</style>
+</head>
+<body>
+    <div class="top"> <h1>Parking Manager : </h1></div>
+    <hr>
+    <div class="mid">
+        <div class="card">
+            <h2>Place 1</h2>
+
+        </div>
+        <div class="card">
+            <h2>Place 2</h2>
+        </div>
+    </div>
+    <footer></footer>
+    
+</body>
+</html>)rawliteral";
+
+  server.setContentLength(1746);  // Permet l'affichage plus rapide après chaque clic sur les boutons
   server.send(200, "text/html", page);
 }
 
@@ -27,42 +97,14 @@ void handleNotFound() { // Page Not found
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Starting BLE work!");
-
-  BLEDevice::init("Long name works now");
-  BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-         
   delay(1000);
-  Serial.println("\n");
-
   WiFi.persistent(false);
   WiFi.begin(ssid, password);
-  Serial.print("Attente de connexion ...");
-  Serial.println("Server started");
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(100);
-  }
-
-  Serial.println("\n");
-  Serial.println("Connexion etablie !");
-  Serial.print("Adresse IP: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("ESP Board MAC Address:  ");
-  Serial.println(WiFi.macAddress());
 
   server.on("/", handleRoot);  // Chargement de la page accueil
   server.onNotFound(handleNotFound);  // Chargement de la page Not found
   server.begin();
 
-  Serial.println("Serveur web actif");
 }
 
 void loop() {
