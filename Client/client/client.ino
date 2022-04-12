799f4bbeea6d1db79b42003a03ccd9d4e4648b6e
RhalisK <khalis.rabehi@etu.univ-lyon1.fr>
2022-04-12 15:06:42 +0200

    a

--- Client/client/client.ino
+++ Client/client/client.ino
@@ -1,153 +1,59 @@
-#include "BLEDevice.h"
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Victor Tchistiak - 2019
+//
+//This example demostrates master mode bluetooth connection and pin 
+//it creates a bridge between Serial and Classical Bluetooth (SPP)
+//this is an extention of the SerialToSerialBT example by Evandro Copercini - 2018
+//
 
-// The remote service we wish to connect to.
-static BLEUUID serviceUUID("4fafc201-1fb5-459e-8fcc-c5c9c331914b");
-// The characteristic of the remote service we are interested in.
-static BLEUUID    charUUID("beb5483e-36e1-4688-b7f5-ea07361b26a8");
+#include "BluetoothSerial.h"
+#include "FeatherShieldPinouts.h"
+BluetoothSerial SerialBT;
 
-static boolean doConnect = false;
-static boolean connected = false;
-static boolean doScan = false;
-static BLERemoteCharacteristic* pRemoteCharacteristic;
-static BLEAdvertisedDevice* myDevice;
-bool IRvalue;
-
-static void notifyCallback(
-  BLERemoteCharacteristic* pBLERemoteCharacteristic,
-  uint8_t* pData,
-  size_t length,
-  bool isNotify) {
-    Serial.print("Notify callback for characteristic ");
-    Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
-    Serial.print(" of data length ");
-    Serial.println(length);
-    Serial.print("data: ");
-    Serial.println((char*)pData);
-}
-
-class MyClientCallback : public BLEClientCallbacks {
-  void onConnect(BLEClient* pclient) {
-  }
-  
-  void onDisconnect(BLEClient* pclient) {
-    connected = false;
-    Serial.println("onDisconnect");
-  }
-};
-
-bool connectToServer() {
-    Serial.print("Forming a connection to ");
-    Serial.println(myDevice->getAddress().toString().c_str());
-    
-    BLEClient*  pClient  = BLEDevice::createClient();
-    Serial.println(" - Created client");
-
-    pClient->setClientCallbacks(new MyClientCallback());
-
-    // Connect to the remove BLE Server.
-    pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
-    Serial.println(" - Connected to server");
-
-    // Obtain a reference to the service we are after in the remote BLE server.
-    BLERemoteService* pRemoteService = pClient->getService(serviceUUID);
-    if (pRemoteService == nullptr) {
-      Serial.print("Failed to find our service UUID: ");
-      Serial.println(serviceUUID.toString().c_str());
-      pClient->disconnect();
-      return false;
-    }
-    Serial.println(" - Found our service");
-
-
-    // Obtain a reference to the characteristic in the service of the remote BLE server.
-    pRemoteCharacteristic = pRemoteService->getCharacteristic(charUUID);
-    if (pRemoteCharacteristic == nullptr) {
-      Serial.print("Failed to find our characteristic UUID: ");
-      Serial.println(charUUID.toString().c_str());
-      pClient->disconnect();
-      return false;
-    }
-    Serial.println(" - Found our characteristic");
-
-    // Read the value of the characteristic.
-    if(pRemoteCharacteristic->canRead()) {
-      std::string value = pRemoteCharacteristic->readValue();
-      Serial.print("The characteristic value was: ");
-      Serial.println(value.c_str());
-    }
-
-    if(pRemoteCharacteristic->canNotify())
-      pRemoteCharacteristic->registerForNotify(notifyCallback);
-
-    connected = true;
-    return true;
-}
-
-class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
- /**
-   * Called for each advertising BLE server.
-   */
-  void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.print("BLE Advertised Device found: ");
-    Serial.println(advertisedDevice.toString().c_str());
-
-    // We have found a device, let us now see if it contains the service we are looking for.
-    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(serviceUUID)) {
-
-      BLEDevice::getScan()->stop();
-      myDevice = new BLEAdvertisedDevice(advertisedDevice);
-      doConnect = true;
-      doScan = true;
-
-    } // Found our server
-  } // onResult
-}; // MyAdvertisedDeviceCallbacks
-
+String MACadd = "3C:61:05:4A:D5:68";
+uint8_t address[6]  = {0x3C, 0x61, 0x05, 0x4A, 0xD5, 0x68};
+//uint8_t address[6]  = {0x00, 0x1D, 0xA5, 0x02, 0xC3, 0x22};
+String name = "Ouistiti";
+char *pin = "1234"; //<- standard pin would be provided by default
+bool connected;
 
 void setup() {
   Serial.begin(115200);
   pinMode(A2,INPUT);
-  Serial.println("Starting Arduino BLE Client application...");
-  BLEDevice::init("");
-
-  // Retrieve a Scanner and set the callback we want to use to be informed when we
-  // have detected a new device.  Specify that we want active scanning and start the
-  // scan to run for 5 seconds.
-  BLEScan* pBLEScan = BLEDevice::getScan();
-  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
-  pBLEScan->setInterval(1349);
-  pBLEScan->setWindow(449);
-  pBLEScan->setActiveScan(true);
-  pBLEScan->start(5, false);
-} // End of setup.
-
-
-// This is the Arduino main loop function.
-void loop() {
-  IRvalue = analogRead(A2);
-  // If the flag "doConnect" is true then we have scanned for and found the desired
-  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are 
-  // connected we set the connected flag to be true.
-  if (doConnect == true) {
-    if (connectToServer()) {
-      Serial.println("We are now connected to the BLE Server.");
-    } else {
-      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+  //SerialBT.setPin(pin);
+  SerialBT.begin("Ouistiti", true); 
+  //SerialBT.setPin(pin);
+  Serial.println("The device started in master mode, make sure remote BT device is on!");
+  
+  // connect(address) is fast (upto 10 secs max), connect(name) is slow (upto 30 secs max) as it needs
+  // to resolve name to address first, but it allows to connect to different devices with the same name.
+  // Set CoreDebugLevel to Info to view devices bluetooth address and device names
+  connected = SerialBT.connect(name);
+  //connected = SerialBT.connect(address);
+  
+  if(connected) {
+    Serial.println("Connected Succesfully!");
+  } else {
+    while(!SerialBT.connected(10000)) {
+      Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app."); 
     }
-    doConnect = false;
   }
+  // this would reconnect to the name(will use address, if resolved) or address used with connect(name/address).
+  SerialBT.connect();
+}
 
-  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
-  // with the current time since boot.
-  if (connected) {
-    String newValue = "Time since boot: " + String(millis()/1000);
-    Serial.println("Setting new characteristic value to \"" + newValue + "\"");
-    
-    // Set the characteristic's value to be the array of bytes that is actually a string.
-    pRemoteCharacteristic->writeValue(newValue.c_str(), newValue.length());
-  }else if(doScan){
-    BLEDevice::getScan()->start(0);  // this is just example to start scan after disconnect, most likely there is better way to do it in arduino
+void loop() {
+  
+  if (digitalRead(A2)){
+    Serial.println("Detect");
+    delay(200);
+  }else{
+    Serial.println("Rien");
+    delay(200);
   }
   
-  delay(1000); // Delay a second between loops.
-} // End of loop
+  if (SerialBT.available()) {
+    Serial.write(digitalRead(A2));
+  }
+  delay(20);
+}
--- Serveur/Test_site/Test_site.ino
+++ Serveur/Test_site/Test_site.ino
@@ -1,112 +1,108 @@
 #include <WiFi.h>  // Librairie Wifi.h
 #include <WebServer.h>  // Librairie WebServer.h
-
-
-const char* ssid = "IPhone de Bob Rasowski";
-const char* password = "azerty01";
+#include "BluetoothSerial.h"
 
 WebServer server(80);
+BluetoothSerial SerialBT;
 
-
-char texteEtatIR[2][10] = {"Libre", "Occupé"}; // Affichage ETEINTE ou ALLUMEE
+//char texteEtatIR[2][10] = {"Libre", "Occupé"}; // Affichage ETEINTE ou ALLUMEE
 
 void handleRoot() {  // Début de la page HTML
-  const char page[] PROGMEM = R"rawliteral(<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="./style.css" rel="stylesheet">
-    <title>Manager</title>
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+   String page  = "<!DOCTYPE html>"
+"<html lang'en'>"
+"<head>"
+    "<meta charset='UTF-8'>"
+    "<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
+    "<meta name='viewport' content='width=device-width, initial-scale=1.0'>"
+    "<link href='./style.css' rel='stylesheet'>"
+    "<title>Manager</title>"
+    "<style>"
+        "@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');"
         
-        *{
-            font-family:'Roboto', sans-serif;;
-            margin: 0;
-            transition: 2s linear;
-        }
+        "*{"
+            "font-family:'Roboto', sans-serif;;"
+           " margin: 0;"
+            "transition: 2s linear;"
+        "}"
         
-        hr{
-            background-color: black;
-            height : 5px;
-            margin : 0;
-        }
+        "hr{"
+            "background-color: black;"
+            "height : 5px;"
+           " margin : 0;"
+       " }"
         
-        .top{
-            background-color: rgb(13, 133, 239);
-            text-align: center;
-            height : 10rem
-        }
+        "."
+       ".top{"
+            "background-color: rgb(13, 133, 239);"
+            "text-align: center;"
+            "height : 10rem"
+        "}"
         
-        .top h1 {
-            padding-top: 15%;
-        }
+        ".top h1 {"
+            "padding-top: 15%;"
+        "}"
         
-        .mid{
-            display: flex;
-            justify-content: space-around;
-            align-items: center;
-            background-color: rgb(222, 220, 220);
-            height : 400px ;
-        }
+        ".mid{"
+            "display: flex;"
+            "justify-content: space-around;"
+            "align-items: center;"
+            "background-color: rgb(222, 220, 220);"
+            "height : 400px ;"
+        "}"
         
-        footer{
-            background-color: black;
-            width : 100%;
-            height: 100px;
-        }
+        "footer{"
+            "background-color: black;"
+            "width : 100%;"
+            "height: 100px;"
+        "}"
         
-        .card{
-            background-color: rgb(202, 200, 200);
-            height : 60%;
-            text-align: center;
-            width : 160px;
-            border-radius: 5%;
-        }
+        ".card{"
+            "background-color: rgb(202, 200, 200);"
+            "height : 60%;"
+            "text-align: center;"
+            "width : 160px;"
+            "border-radius: 5%;"
+        "}"
         
-        .card h2{
-            padding-top: 5%;
-}</style>
-</head>
-<body>
-    <div class="top"> <h1>Parking Manager : </h1></div>
-    <hr>
-    <div class="mid">
-        <div class="card">
-            <h2>Place 1</h2>
+        ".card h2{"
+            "padding-top: 5%;"
+"}</style>"
+"</head>"
+"<body>"
+    "<div class='top'> <h1>Parking Manager : </h1></div>"
+    "<hr>"
+    "<div class='mid'>"
+        "<div class='card'>"
+            "<h2>Place 1</h2>"
 
-        </div>
-        <div class="card">
-            <h2>Place 2</h2>
-        </div>
-    </div>
-    <footer></footer>
+        "</div>"
+        "<div class='card'>"
+            "<h2>Place 2</h2>"
+        "</div>"
+    "</div>"
+    "<footer></footer>"
     
-</body>
-</html>)rawliteral";
-
-  server.setContentLength(1746);  // Permet l'affichage plus rapide après chaque clic sur les boutons
+"</body>"
+"</html>";
   server.send(200, "text/html", page);
-}
-
-void handleNotFound() { // Page Not found
-  server.send(404, "text/plain", "404: Not found");
 }
 
 void setup() {
   Serial.begin(115200);
-  delay(1000);
-  WiFi.persistent(false);
-  WiFi.begin(ssid, password);
 
-  server.on("/", handleRoot);  // Chargement de la page accueil
-  server.onNotFound(handleNotFound);  // Chargement de la page Not found
+  SerialBT.begin("Ouistiti"); //Bluetooth device name
+
+  WiFi.persistent(false);
+  WiFi.begin("IPhone de Bob Rasowski", "azerty01");
+
+  //server.on("/", handleRoot);  // Chargement de la page accueil
   server.begin();
 
 }
 
 void loop() {
   server.handleClient();
+  if (SerialBT.available()) {
+    Serial.println(SerialBT.read());
+  }
 }
