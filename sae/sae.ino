165fbf638bee3fcdbcacdb88fbf83590cdf75d87
simplyy/ <jules.valette2@etu.univ-lyon1.fr>
2022-04-13 14:16:30 +0200

    Update sae.ino

--- sae/sae.ino
+++ sae/sae.ino
@@ -1,36 +1,45 @@
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
-int statusSensor;
+
 
 #include "FeatherShieldPinouts.h"
-Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire);
+#include "BluetoothSerial.h"
+BluetoothSerial SerialBT;
 
 
+uint8_t address[6]  = {0x3C, 0x61, 0x05, 0x4A, 0xD5, 0x68};
+String name = "Ouistiti";
+char *pin = "1234"; //<- standard pin would be provided by default
+bool connected;
 void setup() {
-  display.begin(0x3C, true);
-  display.clearDisplay();
   Serial.begin(115200);
   pinMode(A2, INPUT);
-  pinMode(A0, OUTPUT);
-  display.setTextSize(2); //Set our text size, size 1 correlates to 8pt font
-  display.setTextColor(SH110X_WHITE); //We're using a Monochrome OLED so color is
-  display.setCursor(30, 25); //Start position for the font to appear
-  display.setRotation(3);
-  
+  SerialBT.begin(name, true);
+  Serial.println("Client ON");
+  connected = SerialBT.connect(name);
+  SerialBT.connect();
+  if (connected){
+    Serial.println("Connexion Bluetooth ON");
+  }else{
+     Serial.println("Error Connect, retry incoming");
+     while(!SerialBT.connected(10000)) {
+      Serial.println("Failed to connect. Make sure remote device is available and in range, then restart app."); 
+    }
+    
+  }
+
 }
 
 void loop() {
   delay(200);
-  if (digitalRead(A2)){
+  if (digitalRead(A2)) {
     Serial.println("Detect");
     delay(200);
-  }else{
+  } else {
     Serial.println("Rien");
     delay(200);
   }
-  
-  
-  display.display();
-
+  if (SerialBT.available()){
+    SerialBT.write(digitalRead(A2));
+  }
+  delay(200);
 
 }
